93513dca05e59f2ce0b65a9a98d9e5117e8c89c0
RezMalek <rezmalek7@gmail.com>
2021-04-27 17:55:29 -0700

    add styles

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const cars = [
+  {
+    id: 1,
+    image: 'https://source.unsplash.com/ndiBRRnb7II',
+    photographer: 'Alexandra Nicolae',
+    location: 'San Francisco, CA',
+    quote:
+      'Fingerstache umami squid, kinfolk subway tile selvage tumblr man braid viral kombucha gentrify fanny pack raclette pok pok mustache.',
+  },
+  {
+    id: 2,
+    image: 'https://source.unsplash.com/Fkwj-xk6yck',
+    photographer: 'Ingo Doerrie',
+    location: 'Miami, FL',
+    quote:
+      'Gastropub sustainable tousled prism occupy. Viral XOXO roof party brunch actually, chambray listicle microdosing put a bird on it paleo subway tile squid umami.',
+  },
+  {
+    id: 3,
+    image: 'https://source.unsplash.com/w8CcH9Md4vE',
+    photographer: 'Herson Rodriguez',
+    location: 'San Diego, CA',
+    quote:
+      'Drinking vinegar polaroid street art echo park, actually semiotics next level butcher master cleanse hammock flexitarian ethical paleo.',
+  },
+  {
+    id: 4,
+    image: 'https://source.unsplash.com/9C1tJR1ngTY',
+    photographer: 'Mroux Bulikowska',
+    location: 'New York, NY',
+    quote:
+      'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+  },
+  {
+    id: 5,
+    image: 'https://source.unsplash.com/FTgdUxejEyg',
+    photographer: 'Nathan Dumlao',
+    location: 'Laguna Beach, CA',
+    quote:
+      'Marfa af yr 3 wolf moon kogi, readymade distillery asymmetrical seitan kale chips fingerstache cloud bread mustache twee messenger bag. ',
+  },
+];
+
+export default cars;
